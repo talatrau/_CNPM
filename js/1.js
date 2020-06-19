2d9dad273931313092da23a0eca8fde218e89f5c
hoangpr023 <49335521+hoangpr023@users.noreply.github.com>
2020-06-19 21:38:11 +0700

    kha hoan thien

--- js/1.js
+++ js/1.js
@@ -1,5 +1,5 @@
 function construct() {
-    $(".about-us .title-zone h3").replaceWith(about_us.title);
+    $(".about-us .title-zone h3 span").replaceWith(about_us.title);
     $(".about-us .description").replaceWith(about_us.description)
 }
 if (!String.prototype.format) {
@@ -41,7 +41,7 @@ $(function () {
 });
 
 var about_us = {
-    title: "Appetizer Restaurant",
+    title: "About Us",
     description: "On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word \"and\" and the Little Blind Text should turn around and return to its own, safe country. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.",
 }
 
