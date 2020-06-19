d19073edb7a43543fc4b98d93b9a79fa2121c869
hoang <xuanhoang232k@gmail.com>
2020-06-19 18:26:56 +0700

    second part

---
+++ js/1.js
@@ -0,0 +1,48 @@
+function construct() {
+    $(".about-us .title-zone h3").replaceWith(about_us.title);
+    $(".about-us .description").replaceWith(about_us.description)
+}
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+      var args = arguments;
+      return this.replace(/{(\d+)}/g, function(match, number) { 
+        return typeof args[number] != 'undefined'
+          ? args[number]
+          : match
+        ;
+      });
+    };
+  }
+$(function () {       
+    $('.menu-home').click(function (e) { 
+        e.preventDefault();        
+        pos = 0;
+        $('html').animate({scrollTop: pos}, 1500, "easeOutBack");
+    });
+    $('.menu-news').click(function (e) { 
+        e.preventDefault();   
+        pos = $('.news').offset().top;     
+        $('html').animate({scrollTop: pos}, 1500, "easeOutBack");
+    });
+    $('.menu-menu').click(function (e) { 
+        e.preventDefault();   
+        pos = $('.menu').offset().top;     
+        $('html').animate({scrollTop: pos}, 1500, "easeOutBack");
+    });
+    $('.menu-about').click(function (e) { 
+        e.preventDefault();   
+        pos = $('.about-us').offset().top;     
+        $('html').animate({scrollTop: pos}, 1500, "easeOutBack");
+    });
+    construct();
+    a = JSON.parse(1);
+    console.log(a);
+    
+});
+
+var about_us = {
+    title: "Appetizer Restaurant",
+    description: "On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word \"and\" and the Little Blind Text should turn around and return to its own, safe country. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.",
+}
+
+
